6f241c48a496e5dd9fc374fa0fe7847c38558421
hubert123 <hubert.antoniszyn98@gmail.com>
2022-06-10 15:56:51 +0200

    git commit -m"Update BazaPytanTest"

--- src/test/java/BazaPytanTest.java
+++ src/test/java/BazaPytanTest.java
@@ -1,10 +1,8 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class BazaPytanTest {
 @ParameterizedTest
